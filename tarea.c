a55de92f4172f92f095a4c49222358b9276d0637
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-17 20:52:56 -0500

    Inicio de la tarea de estructuras - LuisPineda

---
+++ tarea.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct alumnos{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char carrera[50];
+    float promedio;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
